6748b220fa69608407d5babcb8f0e7035c4757fe
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-07-19 23:21:32 -0300

    First complex functions

---
+++ src/math/functions/complex_add.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 2017 CMATHL
+
+   This file is part of CML.
+
+   CML is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   CML is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with CML. If not, see <http://www.gnu.org/licenses/>.     */
+
+#include "../../../includes/cml.h"
+
+complex complex_add(complex z, complex w)
+{
+        // Declaration of structures
+        complex r;
+        real n, *m, **x, **y;
+
+        // Mathematic algorithm
+        x = z->parts(z);
+        y = w->parts(w);
+        n = x[0]->add(x[0], y[0]);
+        m = x[1]->add(x[1], y[1]);
+        c = complex_new(n, m);
+
+        // Free structures
+        free(x);
+        free(y);
+        free(n);
+        free(m);
+
+        // Return
+        return r;
+}
