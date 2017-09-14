b5b2ae763d23a66f16e831461ac3d7f6f6b9f2eb
ulises-jeremias <ulisescf.24@gmail.com>
2017-09-14 20:45:53 -0300

    Thread functions

--- includes/bin/cml_threads.h
+++ includes/bin/cml_threads.h
@@ -1,12 +1,19 @@
 #ifndef CML_THREADS_H
 #define CML_THREADS_H
 
+#define MAXLOOPS 100000
+#define MAXTHREADS 10
+
 typedef struct _block {
+        int from;
+        int delta;
         void **argv;
         void *(*function)(void *, ...);
+        void *response;
 } block_t;
 
 block_t block_create(void *(*)(void *, ...), void **);
 void block_destroy(block_t *);
+void series_resolver(block_t *, int, int);
 
 #endif
--- src/bin/cml_threads.c
+++ src/bin/cml_threads.c
@@ -16,14 +16,75 @@
    along with CML. If not, see <http://www.gnu.org/licenses/>.     */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <pthread.h>
 #include "../../includes/bin/cml_threads.h"
+
+static pthread_mutex_t lock;
+double r;
 
 block_t block_create(void *(*function)(void *,...), void **argv)
 {
         block_t block;
         block.function = function;
         block.argv = argv;
+        block.from = 0;
+        block.delta = 0;
         return block;
+}
+
+void block_destroy(block_t *block)
+{
+        free(block);
+}
+
+void *process(void * arg)
+{
+        int i, to;
+        double r;
+        block_t *block;
+        void *(*f)(void *, ...);
+        double *f_r;
+
+        r = 0.0;
+        block = (block_t *) arg;
+        f = block->function;
+        to = block->from + block->delta;
+        for (i = block->from; i < to; i++) {
+                f_r = f(block->argv, i);
+                r = r + *f_r;
+        }
+
+        pthread_mutex_lock(&lock);
+        f_r = block->response;
+        *f_r = r;
+        block->response = f_r;
+        pthread_mutex_unlock(&lock);
+
+        return NULL;
+}
+
+void series_resolver(block_t *block, int max_loops, int max_threads)
+{
+        int i, top, t;
+
+        r = 0.0;
+        top = max_loops == 0 ? MAXLOOPS : max_loops;
+        t = max_threads == 0 ? MAXTHREADS : max_threads;
+
+        pthread_t tid[top/t];
+        pthread_mutex_init(&lock, NULL);
+
+        block->delta = top/t;
+        for (i = 0; i < t; i++) {
+              block->from = (top/t)*i;
+              pthread_create(&(tid[i]), NULL, &process, block);
+        }
+
+        for (i = 0; i < t; i++) {
+              pthread_join(tid[i], NULL);
+        }
+
+        pthread_mutex_destroy(&lock);
 }
