495483ea4f72ba49e82b59482084f9d8ede9b6c9
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-18 17:01:30 -0300

    Updated

--- src/cml.h
+++ src/cml.h
@@ -3,54 +3,58 @@
 
 /* Check C standard */
 #ifdef __STDC__
-  #define PREDEF_STANDARD_C89
-  #ifdef __STDC_VERSION__
-    #if __STDC_VERSION__ >= 199901L
-      #define PREDEF_STANDARD_C99
-    #endif
-  #endif
+        #define PREDEF_STANDARD_C89
+        #ifdef __STDC_VERSION__
+                #if __STDC_VERSION__ >= 199901L
+                        #define PREDEF_STANDARD_C99
+                #endif
+        #endif
 #endif
 
 /* Use `extern inline` for C99 or later */
 #ifdef PREDEF_STANDARD_C99
-#define CML_EXTERN_INLINE extern inline
+        #define CML_EXTERN_INLINE extern inline
 #else
-#define CML_EXTERN_INLINE
+        #define CML_EXTERN_INLINE
 #endif
 
 #include <math/include/math.h>
 
-#ifndef CML_NO_GENERIC
+#ifdef CML_NO_GENERIC
+        #define complex_clone(X) _complex_clone(X)
+        #define complex_new(X, Y) _complex_new(X, Y)
+        #define real_clone(X) _real_clone(X)
+        #define real_new(X) _real_new(X)
+#else
         #define clone(X) _Generic((X), \
                                   complex_t*: _complex_clone, \
                                   real_t*: _real_clone \
                                   )(X)
 
         #define complex_new(X, Y) _Generic((X), \
+                                   default: _Generic((Y), \
+                                             default: _complex_new \
+                                                     ), \
                                            mfloat_t: _Generic((Y), \
                                                               mfloat_t: _complex_new, \
-                                                              real_t*: _complex_new), \
+                                                              real_t*: _complex_new \
+                                                              ), \
                                            real_t*: _Generic((Y), \
                                                              mfloat_t: _complex_new, \
-                                                             real_t*: _complex_new), \
-                                   default: _Generic((Y), \
-                                             default: _complex_new) \
+                                                             real_t*: _complex_new \
+                                                             ) \
                                            )(X, Y)
 
         #define real_new(X) _Generic((X), \
+                             default: _real_new, \
                                      mfloat_t: _real_new, \
-                                     real_t*: _real_clone, \
-                             default: _real_new \
+                                     real_t*: _real_clone \
                                      )(X)
-#else
-        #define complex_clone(X) _complex_clone(X)
-        #define complex_new(X, Y) _complex_new(X, Y)
-        #define real_clone(X) _real_clone(X)
-        #define real_new(X) _real_new(X)
+
 #endif
 
 #ifdef CML_SERIES_RESOLVER
-  #include <utils/include/series_resolver.h>
+        #include <utils/include/series_resolver.h>
 #endif
 
 #endif
