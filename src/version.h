fca04e212e647f7f50709e3ff0543dfc8e9a8aca
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-21 15:48:29 -0300

    Updated version

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef CML_VERSION_H
 #define CML_VERSION_H
 
-#define CML_VERSION 100800U
+#define CML_VERSION 100800100
 
-#define CML_VERSION_STRING "1.8.0"
+#define CML_VERSION_STRING "1.8.1"
 
 #endif
