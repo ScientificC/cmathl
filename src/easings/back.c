0761f07fc1f883141626d0e66487550a9efb867b
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-23 19:59:31 -0300

    Updated

--- src/easings/back.c
+++ src/easings/back.c
@@ -6,29 +6,55 @@
 real
 back_ease_in(real p)
 {
-        mfloat_t f_p = __mfloat__ p;
-        return real(f_p * f_p * f_p - f_p * __mfloat__ sin(real(f_p * PI)));
+        mfloat_t f, g,  f_p = __mfloat__ p;
+        real x, y;
+        x = real(f_p * PI);
+        y = sin(x);
+        g = __mfloat__ y;
+        f = f_p * f_p * f_p - f_p * g;
+
+        free(x);
+        free(y);
+
+        return real(f);
 }
 
 real
 back_ease_out(real p)
 {
-        mfloat_t f = (MFLOAT_T(1.0) - __mfloat__ p);
-        return real(MFLOAT_T(1.0) - (f * f * f - f * __mfloat__ sin(real(f * PI))));
+        mfloat_t g, f = (MFLOAT_T(1.0) - __mfloat__ p);
+        real x, y;
+
+        x = real(f * PI);
+        y = sin(x);
+        g = __mfloat__ y;
+
+        free(x);
+        free(y);
+
+        return real(MFLOAT_T(1.0) - (f * f * f - f * g));
 }
 
 real
 back_ease_in_out(real p)
 {
-        mfloat_t f = MFLOAT_T(0.0), f_p = __mfloat__ p;
+        mfloat_t g, f = MFLOAT_T(0.0), f_p = __mfloat__ p;
+        real x, y;
+
+        x = real(f * PI);
+        y = sin(x);
+        g = __mfloat__ y;
 
         if (f_p < MFLOAT_T(0.5)) {
                 f = MFLOAT_T(2.0) * f_p;
-                f = MFLOAT_T(0.5) * (f * f * f - f * __mfloat__ sin(real(f * PI)));
+                f = MFLOAT_T(0.5) * (f * f * f - f * g);
         } else {
                 f = (MFLOAT_T(1.0) - (MFLOAT_T(2.0) * f_p - MFLOAT_T(1.0)));
-                f = MFLOAT_T(0.5) * (MFLOAT_T(1.0) - (f * f * f - f * __mfloat__ sin(real(f * PI)))) + MFLOAT_T(0.5);
+                f = MFLOAT_T(0.5) * (MFLOAT_T(1.0) - (f * f * f - f * g)) + MFLOAT_T(0.5);
         }
+
+        free(x);
+        free(y);
 
         return real(f);
 }
