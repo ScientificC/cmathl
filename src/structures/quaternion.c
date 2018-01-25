fa5ffd6626cda22933700b94a078e50e5a10f324
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-25 02:06:09 -0300

    Updated double -> real_t

--- src/structures/quaternion.c
+++ src/structures/quaternion.c
@@ -2,23 +2,23 @@
 #include <cml.h>
 
 quaternion_t
-quaternion_create_from_spherical_coords(double vartheta, double varphi) {
-        double ct = cos(vartheta/2.);
-        double cp = cos(varphi/2.);
-        double st = sin(vartheta/2.);
-        double sp = sin(varphi/2.);
+quaternion_create_from_spherical_coords(real_t vartheta, real_t varphi) {
+        real_t ct = cos(vartheta/2.);
+        real_t cp = cos(varphi/2.);
+        real_t st = sin(vartheta/2.);
+        real_t sp = sin(varphi/2.);
 
         return (quaternion_t) { cp*ct, -sp*st, st*cp, sp*ct };
 }
 
 quaternion_t
-quaternion_create_from_euler_angles(double alpha, double beta, double gamma) {
-        double ca = cos(alpha/2.);
-        double cb = cos(beta/2.);
-        double cc = cos(gamma/2.);
-        double sa = sin(alpha/2.);
-        double sb = sin(beta/2.);
-        double sc = sin(gamma/2.);
+quaternion_create_from_euler_angles(real_t alpha, real_t beta, real_t gamma) {
+        real_t ca = cos(alpha/2.);
+        real_t cb = cos(beta/2.);
+        real_t cc = cos(gamma/2.);
+        real_t sa = sin(alpha/2.);
+        real_t sb = sin(beta/2.);
+        real_t sc = sin(gamma/2.);
         return (quaternion_t) {
                        ca*cb*cc-sa*cb*sc,
                             ca*sb*sc-sa*sb*cc,
@@ -30,13 +30,13 @@ quaternion_create_from_euler_angles(double alpha, double beta, double gamma) {
 quaternion_t
 quaternion_sqrt(quaternion_t q)
 {
-        double absolute = quaternion_absolute(q);
+        real_t absolute = quaternion_absolute(q);
         quaternion_t r;
 
         if(abs(1 + q.w/absolute) < _QUATERNION_EPS*absolute) {
                 r = {0.0, 1.0, 0.0, 0.0};
         } else {
-                double c = sqrt(absolute/(2 + 2*q.w/absolute));
+                real_t c = sqrt(absolute/(2 + 2*q.w/absolute));
                 r = {
                         (1.0 + q.w/absolute)*c,
                         q.x*c/absolute,
@@ -51,7 +51,7 @@ quaternion_sqrt(quaternion_t q)
 quaternion_t
 quaternion_log(quaternion_t q)
 {
-        double b = sqrt(q.x*q.x  +  q.y*q.y  +  q.z*q.z);
+        real_t b = sqrt(q.x*q.x  +  q.y*q.y  +  q.z*q.z);
         quaternion_t r;
 
         if(abs(b) <= _QUATERNION_EPS*abs(q.w)) {
@@ -68,21 +68,21 @@ quaternion_log(quaternion_t q)
                         r = {log(q.w), 0., 0., 0.};
                 }
         } else {
-                double v = atan2(b, q.w);
-                double f = v/b;
+                real_t v = atan2(b, q.w);
+                real_t f = v/b;
                 r = { log(q.w*q.w + b*b)/2.0, f*q.x, f*q.y, f*q.z };
         }
 
         return r;
 }
 
-double
-_quaternion_scalar_log(double s) {
+real_t
+_quaternion_scalar_log(real_t s) {
         return log(s);
 }
 
 quaternion_t
-quaternion_scalar_power(double s, quaternion_t q)
+quaternion_scalar_power(real_t s, quaternion_t q)
 {
         /* Unlike the quaternion^quaternion power, this is unambiguous.*/
         if(is_null(s)) { /*log(s)=-inf */
@@ -104,12 +104,12 @@ quaternion_scalar_power(double s, quaternion_t q)
 quaternion_t
 quaternion_exp(quaternion_t q)
 {
-        double vnorm = sqrt(q.x*q.x  +  q.y*q.y  +  q.z*q.z);
+        real_t vnorm = sqrt(q.x*q.x  +  q.y*q.y  +  q.z*q.z);
         quaternion_t r;
 
         if (vnorm > _QUATERNION_EPS) {
-                double s = sin(vnorm) / vnorm;
-                double e = exp(q.w);
+                real_t s = sin(vnorm) / vnorm;
+                real_t e = exp(q.w);
                 r = {e*cos(vnorm), e*s*q.x, e*s*q.y, e*s*q.z};
         } else {
                 r = { exp(q.w), 0, 0, 0 };
