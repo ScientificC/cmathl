8caddac5d578fb06cfd3bef84a777ac23d272b29
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-27 13:35:21 -0300

    Updated easings header files

--- src/cml/easings.h
+++ src/cml/easings.h
@@ -5,6 +5,16 @@
 #ifndef CML_EASIGNS_H
 #define CML_EASIGNS_H
 
-#include "easings/include/functions.h"
+#include "easings/include/back.h"
+#include "easings/include/bounce.h"
+#include "easings/include/circular.h"
+#include "easings/include/cubic.h"
+#include "easings/include/elastic.h"
+#include "easings/include/exponential.h"
+#include "easings/include/linear.h"
+#include "easings/include/quadratic.h"
+#include "easings/include/quartic.h"
+#include "easings/include/quintic.h"
+#include "easings/include/sine.h"
 
 #endif
