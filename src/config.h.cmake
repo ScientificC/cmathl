6cba32b149013808224d35cb5c6e243eaba200ca
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-28 16:47:32 -0300

    Updated

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -1,9 +1,6 @@
 #ifndef CML_CONFIG_H
 #define CML_CONFIG_H
 
-#cmakedefine mfloat_t double
-#cmakedefine CML_DOUBLE_PRECISION 1
-
 #define CML_VERSION "@PROJECT_VERSION@"
 
 #endif /* CML_CONFIG_H */
