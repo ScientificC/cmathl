4a0ae0e8a2fd7a6a2f335f09f6cbdac9093bcfa8
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-28 01:39:11 -0300

    Updated

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #ifndef CML_CONFIG_H
 #define CML_CONFIG_H
 
-#define mfloat_t double
-#define CML_DOUBLE_PRECISION
+/* #undef mfloat_t */
+/* #undef CML_DOUBLE_PRECISION */
 
 #define CML_VERSION "1.9.2.0"
 
--- src/config.h.cmake
+++ src/config.h.cmake
@@ -2,7 +2,7 @@
 #define CML_CONFIG_H
 
 #cmakedefine mfloat_t double
-#cmakedefine CML_DOUBLE_PRECISION
+#cmakedefine CML_DOUBLE_PRECISION 1
 
 #define CML_VERSION "@PROJECT_VERSION@"
 
