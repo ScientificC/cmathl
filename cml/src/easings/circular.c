765824b59566c6f251203c9cb9623a797252a816
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-31 08:39:54 -0300

    Updated easings

--- cml/src/easings/circular.c
+++ cml/src/easings/circular.c
@@ -6,13 +6,13 @@
 real_t
 circular_ease_in(real_t p)
 {
-        return (1.0) - sqrt((1.0) - (p * p));
+        return (1.0) - real_sqrt((1.0) - (p * p));
 }
 
 real_t
 circular_ease_out(real_t p)
 {
-        return sqrt(((2.0) - p) * p);
+        return real_sqrt(((2.0) - p) * p);
 }
 
 real_t
@@ -21,9 +21,9 @@ circular_ease_in_out(real_t p)
         real_t f = (0.0);
 
         if (p < (0.5)) {
-                f = (0.5) * ((1.0) - sqrt((1.0) - (4.0) * (p * p)));
+                f = (0.5) * ((1.0) - real_sqrt((1.0) - (4.0) * (p * p)));
         } else {
-                f = (0.5) * (sqrt(-(((2.0) * p) - (3.0)) * (((2.0) * p) - (1.0))) + (1.0));
+                f = (0.5) * (real_sqrt(-(((2.0) * p) - (3.0)) * (((2.0) * p) - (1.0))) + (1.0));
         }
 
         return f;
