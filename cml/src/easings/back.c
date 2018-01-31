6d2c086c9cb0ea23535e7300e6d9b13a69e73140
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-31 08:39:34 -0300

    Updated easings

--- cml/src/easings/back.c
+++ cml/src/easings/back.c
@@ -6,14 +6,14 @@
 real_t
 back_ease_in(real_t p)
 {
-        return p * p * p - p * sin(p * PI);
+        return p * p * p - p * real_sin(p * PI);
 }
 
 real_t
 back_ease_out(real_t p)
 {
         real_t f = ((1.0) - p);
-        return (1.0) - (f * f * f - f * sin(f * PI));
+        return (1.0) - (f * f * f - f * real_sin(f * PI));
 }
 
 real_t
@@ -23,10 +23,10 @@ back_ease_in_out(real_t p)
 
         if (p < (0.5)) {
                 f = (2.0) * p;
-                f = (0.5) * (f * f * f - f * sin(f * PI));
+                f = (0.5) * (f * f * f - f * real_sin(f * PI));
         } else {
                 f = ((1.0) - ((2.0) * p - (1.0)));
-                f = (0.5) * ((1.0) - (f * f * f - f * sin(f * PI))) + (0.5);
+                f = (0.5) * ((1.0) - (f * f * f - f * real_sin(f * PI))) + (0.5);
         }
 
         return f;
--- cml/src/easings/exponential.c
+++ cml/src/easings/exponential.c
@@ -9,7 +9,7 @@ exponential_ease_in(real_t p)
         real_t f = p;
 
         if (p != (0.0)) {
-                f = pow((2.0), (10.0) * (p - (1.0)));
+                f = real_pow((2.0), (10.0) * (p - (1.0)));
         }
 
         return f;
@@ -21,7 +21,7 @@ exponential_ease_out(real_t p)
         real_t f = p;
 
         if (p != (1.0)) {
-                f = (1.0) - pow((2.0), -(10.0) * p);
+                f = (1.0) - real_pow((2.0), -(10.0) * p);
         }
 
         return f;
@@ -33,9 +33,9 @@ exponential_ease_in_out(real_t p)
         real_t f = p;
 
         if (p < (0.5)) {
-                f = (0.5) * pow((2.0), ((20.0) * p) - (10.0));
+                f = (0.5) * real_pow((2.0), ((20.0) * p) - (10.0));
         } else {
-                f = -(0.5) * pow((2.0), (-(20.0) * p) + (10.0)) + (1.0);
+                f = -(0.5) * real_pow((2.0), (-(20.0) * p) + (10.0)) + (1.0);
         }
 
         return f;
