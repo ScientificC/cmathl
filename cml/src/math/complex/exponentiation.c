3052abfe2208910b9185c4206636b3303de712df
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-05 00:31:01 -0300

    Added exponentiantion functions

--- cml/src/math/complex/exponentiation.c
+++ cml/src/math/complex/exponentiation.c
@@ -71,3 +71,106 @@ complex_log(complex_t z)
         /* Return */
         return w;
 }
+
+
+complex_t
+complex_sqrt(complex_t a)
+{                               /* z=sqrt(a) */
+        complex_t z;
+
+        if (real_isnull(a.re) && real_isnull(a.im)) {
+                complex(0.0, 0.0);
+        } else {
+                real_t x = real_abs(a.re);
+                real_t y = real_abs(a.im);
+                real_t w;
+
+                if (x >= y) {
+                        real_t t = y / x;
+                        w = real_sqrt(x) * real_sqrt(0.5 * (1.0 + real_sqrt(1.0 + t * t)));
+                } else {
+                        real_t t = x / y;
+                        w = real_sqrt(y) * real_sqrt(0.5 * (t + real_sqrt(1.0 + t * t)));
+                }
+
+                if (a.re >= 0.0) {
+                        real_t ai = a.im;
+                        z = complex(w, ai / (2.0 * w));
+                } else {
+                        real_t ai = a.im;
+                        real_t vi = (ai >= 0) ? w : -w;
+                        z = complex(ai / (2.0 * vi), vi);
+                }
+        }
+
+        return z;
+}
+
+
+complex_t
+complex_sqrt_real(real_t x)
+{                               /* z=sqrt(x) */
+        complex_t z;
+
+        if (x >= 0) {
+                z = complex(real_sqrt(x), 0.0);
+        } else {
+                z = complex(0.0, real_sqrt(-x));
+        }
+
+        return z;
+}
+
+
+complex_t
+complex_pow (complex_t a, complex_t b)
+{                               /* z=a^b */
+        complex_t z;
+
+        if (real_isnull(a.re) && real_isnull(a.im)) {
+                if (real_isnull(b.re) && real_isnull(b.im)) {
+                        z = complex(1.0, 0.0);
+                } else {
+                        z = complex(0.0, 0.0);
+                }
+        } else if (real_isnull(b.re) && real_isnull(b.im)) {
+                return a;
+        } else if (real_equal(b.re, -1.0) && real_isnull(b.im)) {
+                return complex_inverse(a);
+        } else {
+                real_t logr = real_log(complex_abs(a));
+                real_t theta = complex_arg(a);
+
+                real_t br = b.re, bi = b.im;
+
+                real_t rho = real_exp(logr * br - bi * theta);
+                real_t beta = theta * br + bi * logr;
+
+                z = complex(rho * real_cos(beta), rho * real_sin(beta));
+        }
+
+        return z;
+}
+
+
+complex_t
+complex_pow_real (complex_t a, real_t b)
+{                               /* z=a^b */
+        complex_t z;
+
+        if (real_isnull(a.re) && real_isnull(a.im)) {
+                if (real_isnull(b)) {
+                        z = complex(1.0, 0.0);
+                } else {
+                        z = complex(0.0, 0.0);
+                }
+        } else {
+                real_t logr = real_log(complex_abs(a));
+                real_t theta = complex_arg(a);
+                real_t rho = real_exp(logr * b);
+                real_t beta = theta * b;
+                z = complex(rho * real_cos(beta), rho * real_sin(beta));
+        }
+
+        return z;
+}
