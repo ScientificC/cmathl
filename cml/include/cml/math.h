d0dddd05a70d1377a2e94831e0d9d4b051b922c7
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-14 14:10:18 -0300

    Added math function definition

--- cml/include/cml/math.h
+++ cml/include/cml/math.h
@@ -29,4 +29,50 @@
 #include "math/real.h"
 #include "math/complex.h"
 
+__CML_BEGIN_DECLS
+
+/* Definition of an arbitrary function with parameters */
+
+struct function_struct
+{
+        double (* function) (double x, void * params);
+        void * params;
+};
+
+typedef struct function_struct function_t;
+
+#define FN_EVAL(F,x) (*((F)->function))(x,(F)->params)
+
+
+/* Definition of an arbitrary function returning two values, r1, r2 */
+
+struct function_fdf_struct
+{
+        double (* f) (double x, void * params);
+        double (* df) (double x, void * params);
+        void (* fdf) (double x, void * params, double * f, double * df);
+        void * params;
+};
+
+typedef struct function_fdf_struct function_fdf_t;
+
+#define FN_FDF_EVAL_F(FDF,x) (*((FDF)->f))(x,(FDF)->params)
+#define FN_FDF_EVAL_DF(FDF,x) (*((FDF)->df))(x,(FDF)->params)
+#define FN_FDF_EVAL_F_DF(FDF,x,y,dy) (*((FDF)->fdf))(x,(FDF)->params,(y),(dy))
+
+
+/* Definition of an arbitrary vector-valued function with parameters */
+
+struct function_vec_struct
+{
+        int (* function) (double x, double y[], void * params);
+        void * params;
+};
+
+typedef struct function_vec_struct function_vec_t;
+
+#define FN_VEC_EVAL(F,x,y) (*((F)->function))(x,y,(F)->params)
+
+__CML_END_DECLS
+
 #endif
