d3384753778e5e2d40a8d80a8feb9091002578d7
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-16 01:19:34 -0300

    Updated cml inline header file

--- cml/include/cml/inline.h
+++ cml/include/cml/inline.h
@@ -15,11 +15,6 @@
 #ifdef PREDEF_STANDARD_C99
         #define __CML_EXTERN_INLINE extern __CML_INLINE
 #else
-        #undef __CML_EXTERN_INLINE
-        #undef CML_SINGLE_PRECISION
-        #undef mfloat_t
-
         #define __CML_EXTERN_INLINE
-        #define mfloat_t double
 #endif
 #endif
