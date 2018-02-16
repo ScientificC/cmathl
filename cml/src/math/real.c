c144ee7fa0bd5e46ec8549ae40115c65e21b3b42
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-16 00:35:37 -0300

    Updated

--- cml/src/math/real.c
+++ cml/src/math/real.c
@@ -27,26 +27,24 @@ real_ninf()
 char *
 real_as_string(real_t x)
 {
-        mfloat_t value = x;
-
         #ifndef PREDEF_STANDARD_C99
         char * string = (char*) malloc(80);
         #ifndef CML_NO_LONG_DOUBLE_MATH
-        sprintf(string, "%Lg", value);
+        sprintf(string, "%Lg", x);
         #else
-        sprintf(string, "%g", value);
+        sprintf(string, "%g", x);
         #endif
         #else
         #ifndef CML_NO_LONG_DOUBLE_MATH
-        int size = snprintf(NULL, 0, "%Lg", value) + 1;
+        int size = snprintf(NULL, 0, "%Lg", x) + 1;
         #else
-        int size = snprintf(NULL, 0, "%g", value) + 1;
+        int size = snprintf(NULL, 0, "%g", x) + 1;
         #endif
         char * string = (char*) malloc(size);
         #ifndef CML_NO_LONG_DOUBLE_MATH
-        snprintf(string, size, "%Lg", value);
+        snprintf(string, size, "%Lg", x);
         #else
-        snprintf(string, size, "%g", value);
+        snprintf(string, size, "%g", x);
         #endif
         #endif
 
