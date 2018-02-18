c3d949456a9a8f0658b5c5a6a2675b30a51f4402
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-17 22:03:06 -0300

    Added real classification functions

--- cml/src/math/real/classification.c
+++ cml/src/math/real/classification.c
@@ -1,5 +1,28 @@
 #include <stdlib.h>
+#include <math.h>
+
+#undef CML_NO_ALIASES
+#define CML_NO_ALIASES
+
 #include <cml.h>
+
+#ifndef PREDEF_STANDARD_C99
+/* FIXME: classification functions are not ANSI C complaint, should be compatible
+   with c89 and c99. */
+
+        #ifndef isnan
+                #define isnan(X) __isnan(X)
+        #endif
+
+        #ifndef isinf
+                #define isinf(X) __isinf(X)
+        #endif
+
+        #ifndef isfinite
+                #define isfinite(X) __finite(X)
+        #endif
+#endif
+
 
 __CML_EXTERN_INLINE bool
 __isinteger(real_t x)
@@ -26,6 +49,13 @@ real_equal(real_t x, real_t y)
 
 
 bool
+real_isfinite(real_t x)
+{
+        return isfinite(x);
+}
+
+
+bool
 real_isgreaterequal(real_t x, real_t y)
 {
         return real_isgreater(x, y) || real_equal(x, y);
@@ -36,6 +66,13 @@ bool
 real_isgreater(real_t x, real_t y)
 {
         return x > y;
+}
+
+
+bool
+real_isinf(real_t x)
+{
+        return isinf(x);
 }
 
 
@@ -71,6 +108,13 @@ bool
 real_isnatural(real_t x)
 {
         return real_isinteger(x) && x > 0.0;
+}
+
+
+bool
+real_isnan(real_t x)
+{
+        return isnan(x);
 }
 
 
--- docs/REFERENCE.md
+++ docs/REFERENCE.md
@@ -164,13 +164,16 @@ real_t real_floor(real_t);
 
 ```c
 bool real_equal(real_t, real_t);
+bool real_isfinite(real_t);
 bool real_isgreaterequal(real_t, real_t);
 bool real_isgreater(real_t, real_t);
+bool real_isinf(real_t);
 bool real_isinteger(real_t);
 bool real_islessequal(real_t, real_t);
 bool real_isless(real_t, real_t);
 bool real_ismult(real_t, real_t);
 bool real_isnatural(real_t);
+bool real_isnan(real_t);
 bool real_isnull(real_t);
 ```
 
