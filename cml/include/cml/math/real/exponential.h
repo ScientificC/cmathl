b9fb062e331ed24f29580f3aead6b263c92b2043
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-22 20:20:01 -0300

    Added exp functions

--- cml/include/cml/math/real/exponential.h
+++ cml/include/cml/math/real/exponential.h
@@ -4,6 +4,9 @@
 __CML_BEGIN_DECLS
 
 real_t real_exp(real_t);
+real_t real_expm1(real_t x);
+real_t real_ldexp(real_t x, int e);
+real_t real_frexp(real_t x, int *e);
 real_t real_log(real_t);
 real_t real_log_b(real_t, real_t);
 real_t real_log1p(real_t);
