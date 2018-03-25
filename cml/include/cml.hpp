c396f7864798e633bf0b62d67302474191cdad46
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-25 20:25:33 -0300

    Updated

--- cml/include/cml.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _CML_H_PP
-#define _CML_H_PP
-
-namespace cml {
-            #include <cml.h>
-}
-
-#endif
--- cml/include/config.h.cmake
+++
@@ -1,6 +0,0 @@
-#ifndef CML_CONFIG_H
-#define CML_CONFIG_H
-
-#define CML_VERSION "@PROJECT_VERSION@"
-
-#endif /* CML_CONFIG_H */
