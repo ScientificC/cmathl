492200386fdd7d93e474560e9017b0c9222a51b5
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-26 17:48:36 -0300

    Added block read modules

--- cml/src/block/source/fprintf.c
+++ cml/src/block/source/fprintf.c
@@ -45,37 +45,6 @@ FUNCTION(cml_block, fprintf)(FILE *stream, const TYPE(cml_block) *b, const char 
         return 0;
 }
 
-int
-FUNCTION(cml_block, fscanf)(FILE *stream, TYPE(cml_block) *b)
-{
-        size_t n = b->size;
-
-        ATOMIC *data = b->data;
-
-        size_t i;
-
-        for (i = 0; i < n; i++)
-        {
-                int k;
-                for (k = 0; k < MULTIPLICITY; k++)
-                {
-                        ATOMIC_IO tmp;
-
-                        int status = fscanf(stream, IN_FORMAT, &tmp);
-
-                        data[MULTIPLICITY * i + k] = tmp;
-
-
-                        if (status != 1)
-                        {
-                                CML_ERROR("fscanf failed", CML_EFAILED);
-                        }
-                }
-        }
-
-        return CML_SUCCESS;
-}
-
 
 int
 FUNCTION(cml_block, raw_fprintf)(FILE *stream,
@@ -122,33 +91,5 @@ FUNCTION(cml_block, raw_fprintf)(FILE *stream,
         return 0;
 }
 
-int
-FUNCTION(cml_block, raw_fscanf)(FILE *stream,
-                                ATOMIC *data,
-                                const size_t n,
-                                const size_t stride)
-{
-        size_t i;
-
-        for (i = 0; i < n; i++)
-        {
-                int k;
-                for (k = 0; k < MULTIPLICITY; k++)
-                {
-                        ATOMIC_IO tmp;
-
-                        int status = fscanf(stream, IN_FORMAT, &tmp);
-
-                        data[MULTIPLICITY * i * stride + k] = tmp;
-
-                        if (status != 1)
-                        {
-                                CML_ERROR("fscanf failed", CML_EFAILED);
-                        }
-                }
-        }
-
-        return CML_SUCCESS;
-}
 
 #endif
