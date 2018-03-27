752ff41c621db73c13a6059cc1248a2ef5f4c50f
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-27 00:12:49 -0300

    Updated qsort

--- cml/include/cml/sort/utils.h
+++ cml/include/cml/sort/utils.h
@@ -5,7 +5,7 @@ __CML_BEGIN_DECLS
 
 typedef int (*cml_comparison_fn_t)(const void *, const void *);
 
-void cml_qsort(void *a, int n, int es, int lda, int *t, int ldt, int index_flag, cml_comparison_fn_t cmp);
+void cml_qsort(void *a, size_t n, size_t es, cml_comparison_fn_t cmp);
 
 void cml_heapsort(void * array, size_t count, size_t size, cml_comparison_fn_t compare);
 int cml_heapsort_index(size_t * p, const void * array, size_t count, size_t size, cml_comparison_fn_t compare);
--- cml/src/sort/qsort.c
+++ cml/src/sort/qsort.c
@@ -33,9 +33,7 @@
 
 /*
  * This file has been modified by Ulises Jeremias Cornejo Fandos
- * to store the permutation vector resulting from the sorting
- * and to sort array with non contiguous cells,
- * typically a column of matrix sorted row-wise.
+ * in order to make the code compatible with the library.
  */
 
 #include <sys/types.h>
@@ -50,281 +48,149 @@
  * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
  */
 
-/* this function takes care of a possible leading dimension. This means to
- * between two cells in a, we have to jump of lda * es
- * es is the size of one cell of a
- * n is the length of a
- * lda the leading dimension
+static __CML_INLINE char  *med3(char *, char *, char *, cml_comparison_fn_t);
+static __CML_INLINE void   swapfunc(char *, char *, size_t, int);
+
+/*
+ * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
  */
-static __CML_INLINE void
-swapfunc(char *a, char *b, int es, int n, int lda)
-{
-        long i = n, atomic_size;
-        register char *pi = a;
-        register char *pj = b;
+#define swapcode(TYPE, parmi, parmj, n) {     \
+                size_t i = (n) / sizeof (TYPE);       \
+                TYPE *pi = (TYPE *) (parmi);    \
+                TYPE *pj = (TYPE *) (parmj);    \
+                do {            \
+                        TYPE t = *pi;    \
+                        *pi++ = *pj;        \
+                        *pj++ = t;        \
+                } while (--i > 0);        \
+}
 
-        do
+#define SWAPINIT(a, es) swaptype = ((char *)a - (char *)0) % sizeof(long) || \
+                                   es % sizeof(long) ? 2 : es == sizeof(long)? 0 : 1;
+
+static __CML_INLINE void
+swapfunc(char *a, char *b, size_t n, int swaptype)
+{
+        if(swaptype <= 1)
         {
-                atomic_size = es;
-                do
-                {
-                        register char temp = *pi;
-                        *pi = *pj;
-                        *pj = temp;
-                        pi++;
-                        pj++;
-                }
-                while (--atomic_size > 0);
-                pi--;
-                pj--;
-                pi += lda;
-                pj += lda;
+                swapcode(long, a, b, n);
         }
-        while (--i > 0);
-}
-
-
-static __CML_INLINE void
-swapfunc_double(double *a, double *b, int n, int lda)
-{
-        register double *pi = a;
-        register double *pj = b;
-        do
+        else
         {
-                register double temp = *pi;
-                *pi = *pj;
-                *pj = temp;
-                pi += lda;
-                pj += lda;
+                swapcode(char, a, b, n);
         }
-        while (--n > 0);
 }
 
+#define swap(a, b)          \
+        if (swaptype == 0) {        \
+                long t = *(long *)(a);      \
+                *(long *)(a) = *(long *)(b);    \
+                *(long *)(b) = t;     \
+        } else            \
+                swapfunc(a, b, es, swaptype)
 
-static __CML_INLINE void
-swapfunc_int(int *a, int *b, int n, int lda)
+#define vecswap(a, b, n)  if ((n) > 0) swapfunc(a, b, n, swaptype)
+
+static __CML_INLINE char *
+med3(char *a, char *b, char *c, cml_comparison_fn_t cmp )
 {
-        register int *pi = a;
-        register int *pj = b;
-        do
-        {
-                register int temp = *pi;
-                *pi = *pj;
-                *pj = temp;
-                pi += lda;
-                pj += lda;
-        }
-        while (--n > 0);
+        return cmp(a, b) < 0 ?
+               (cmp(b, c) < 0 ? b : (cmp(a, c) < 0 ? c : a ))
+               : (cmp(b, c) > 0 ? b : (cmp(a, c) < 0 ? a : c ));
 }
 
+/*  The qsort function sorts an array of nmemb objects, the initial element of
+    which is pointed to by base.  The size of each object is specified by size.
 
-#define swap2(a, b, TYPE) {register TYPE temp; temp = *((TYPE *)(a));   \
-                           *((TYPE *)(a)) = *((TYPE *)(b)); *((TYPE *)(b)) = temp; }
+    The contents of the array are sorted into ascending order according to a
+    comparison function pointed to by compar, which is called with two
+    arguments that point to the objects being compared. The function shall
+    return an integer less than, equal to, or greater than zero if the first
+    argument is considered to be respectively less than, equal to, or greater
+    than the second.
 
-#define swap(a, b) if (es==sizeof(int)) swap2(a, b, int)    \
-                else if (es==sizeof(double)) swap2 (a, b, double)       \
-                        else swapfunc(a, b, es, 1, 1);
-
-#define swapindex(a,b) if (index_flag==true) {register int temp; temp = *(a); *(a) = *(b); *(b) = temp; }
-
-
-#define vecswap(a, b, n, lda)    if ((n) > 0) {                         \
-                es==sizeof(int) ? swapfunc_int((int *)(a), (int *)(b), (n)/es, lda) : \
-                (es==sizeof(double) ? swapfunc_double((double *)(a), (double *)(b), (n)/es, lda) : \
-                 swapfunc(a, b, es, n/es, lda));                                  \
-}
-
-/* #define vecswap(a, b, n, lda)    if ((n) > 0) { swapfunc(a, b, es, n/es, lda); } */
-
-#define vecswapindex(a, b, n, lda)    if ((n) > 0 && index_flag==true) swapfunc_int(a, b, n, lda)
-
-/* static __CML_INLINE char *med3(char *a, char *b, char *c, int (*cmp)(const void*, const void *)) */
-/* { */
-/*   return cmp(a, b) < 0 ? */
-/*     (cmp(b, c) < 0 ? b : (cmp(a, c) < 0 ? c : a )) */
-/*     :(cmp(b, c) > 0 ? b : (cmp(a, c) < 0 ? a : c )); */
-/* } */
-
-
-static __CML_INLINE void
-med3withindex(char *ans, int *i_ans, char *a, char *b, char *c,
-              int *i_a, int *i_b, int *i_c, cml_comparison_fn_t cmp)
-{
-        cmp(a, b) < 0 ?
-        (cmp(b, c) < 0 ? (ans = b, i_ans = i_b) :
-         (cmp(a, c) < 0 ? (ans = c, i_ans = i_c) :
-          (ans = a, i_ans = i_a)))
-        : (cmp(b, c) > 0 ? (ans = b, i_ans = i_b) :
-           (cmp(a, c) < 0 ? (ans = a, i_ans = i_a) :
-            (ans = c, i_ans = i_c)));
-
-        (void) ans;
-}
-
-
-static __CML_INLINE void
-med3noindex(char *ans, int *i_ans, char *a, char *b, char *c,
-            int *i_a, int *i_b, int *i_c, cml_comparison_fn_t cmp)
-{
-        cmp(a, b) < 0 ? (cmp(b, c) < 0 ? (ans = b) : (cmp(a, c) < 0 ? (ans = c) : (ans = a)))
-        : (cmp(b, c) > 0 ? (ans = b) : (cmp(a, c) < 0 ? (ans = a) : (ans = c)));
-
-        (void) ans;
-        (void) i_ans;
-        (void) i_a;
-        (void) i_b;
-        (void) i_c;
-}
-
-#define med3index (index_flag==true ? med3withindex : med3noindex)
-
-/**
- * @param a a generic pointer to be sorted
- * @param n the length of the array a
- * @param es the size of an element of a
- * @param lda the leading dimension of a, used to sort columns of matrices
- * stored row-wise
- * @param t an array of integers to store the permutation used to sort a
- * @param ldt the leading dimension of t, used to sort columns of matrices
- * stored row-wise
- * @param index_flag can be true or false. If true the permutation vector is
- * computed. If false the parameter t is not used
- * @param cmp the comparaison function
+    If two elements compare as equal, their order in the resulting sorted array
+    is unspecified.
  */
 void
-cml_qsort(void *a, int n, int es, int lda, int *t, int ldt, int index_flag,
-          cml_comparison_fn_t cmp)
+cml_qsort(void *a, size_t n, size_t es, cml_comparison_fn_t cmp)
 {
         char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
-        int d, swap_cnt;
-        int *ta, *tb, *tc, *td, *tm, *tl, *tn, d_t;
-        int esa, est;
-        ptrdiff_t r, r_t;
+        size_t d, r;
+        int cmp_result;
+        int swaptype, swap_cnt;
 
-        esa = es * lda;
-        est = ldt;
-
-loop:
+loop:   SWAPINIT(a, es);
         swap_cnt = 0;
-        if (n < 7)
-        {
-                for (pm = ((char *)a) + esa, tm = t + est; pm < ((char *) a) + n * esa; pm += esa, tm += est)
-                {
-                        for (pl = pm, tl = tm; pl > (char *) a && cmp(pl - esa, pl) > 0; pl -= esa, tl -= est)
-                        {
-                                swap(pl, pl - esa);
-                                swapindex(tl, tl - est);
-                        }
-                }
+        if (n < 7) {
+                for (pm = (char *)a + es; pm < (char *)a + n * es; pm += es)
+                        for (pl = pm;
+                             pl > (char *)a && cmp(pl - es, pl) > 0;
+                             pl -= es)
+                                swap(pl, pl - es);
                 return;
         }
-        pm = ((char *) a) + (n / 2) * esa;
-        tm = t + (n / 2) * est;
-        if (n > 7)
-        {
+        pm = (char *)a + (n / 2) * es;
+        if (n > 7) {
                 pl = a;
-                tl = t;
-                pn = ((char *) a) + (n - 1) * esa;
-                tn = t + (n - 1) * est;
-                if (n > 40)
-                {
-                        d = (n / 8) * esa;
-                        d_t = (n / 8) * est;
-                        med3index(pl, tl, pl, pl + d, pl + 2 * d, tl, tl + d_t, tl + 2 * d_t, cmp);
-                        med3index(pm, tm, pm - d, pm, pm + d, tm - d_t, tm, tm + d_t, cmp);
-                        med3index(pn, tn, pn - 2 * d, pn - d, pn, tn - 2 * d_t, tn - d_t, tn, cmp);
+                pn = (char *)a + (n - 1) * es;
+                if (n > 40) {
+                        d = (n / 8) * es;
+                        pl = med3(pl, pl + d, pl + 2 * d, cmp);
+                        pm = med3(pm - d, pm, pm + d, cmp);
+                        pn = med3(pn - 2 * d, pn - d, pn, cmp);
                 }
-                med3index(pm, tm, pl, pm, pn, tl, tm, tn, cmp);
+                pm = med3(pl, pm, pn, cmp);
         }
         swap(a, pm);
-        swapindex(t, tm);
+        pa = pb = (char *)a + es;
 
-        pa = pb = ((char *) a) + esa;
-        pc = pd = ((char *)a) + (n - 1) * esa;
-
-        ta = tb = t + est;
-        tc = td = t + (n - 1) * est;
-
-        for (;;)
-        {
-                while (pb <= pc && (r = cmp(pb, a)) <= 0)
-                {
-                        if (r == 0)
-                        {
+        pc = pd = (char *)a + (n - 1) * es;
+        for (;;) {
+                while (pb <= pc && (cmp_result = cmp(pb, a)) <= 0) {
+                        if (cmp_result == 0) {
                                 swap_cnt = 1;
                                 swap(pa, pb);
-                                pa += esa;
-                                swapindex(ta, tb);
-                                ta += est;
+                                pa += es;
                         }
-                        pb += esa;
-                        tb += est;
+                        pb += es;
                 }
-                while (pb <= pc && (r = cmp(pc, a)) >= 0)
-                {
-                        if (r == 0)
-                        {
+                while (pb <= pc && (cmp_result = cmp(pc, a)) >= 0) {
+                        if (cmp_result == 0) {
                                 swap_cnt = 1;
                                 swap(pc, pd);
-                                pd -= esa;
-                                swapindex(tc, td);
-                                td -= est;
+                                pd -= es;
                         }
-                        pc -= esa;
-                        tc -= est;
+                        pc -= es;
                 }
                 if (pb > pc)
                         break;
                 swap(pb, pc);
-                swapindex(tb, tc);
                 swap_cnt = 1;
-                pb += esa;
-                pc -= esa;
-                tb += est;
-                tc -= est;
+                pb += es;
+                pc -= es;
         }
-        if (swap_cnt == 0) /* Switch to insertion sort */
-        {
-                for (pm = ((char *)a) + esa, tm = t + est; pm < ((char *) a) + n * esa; pm += esa, tm += est)
-                {
-                        for (pl = pm, tl = tm; pl > (char *) a && cmp(pl - esa, pl) > 0;
-                             pl -= esa, tl -= est)
-                        {
-                                swap(pl, pl - esa);
-                                swapindex(tl, tl - est);
-                        }
-                }
+        if (swap_cnt == 0) { /* Switch to insertion sort */
+                for (pm = (char *)a + es; pm < (char *)a + n * es; pm += es)
+                        for (pl = pm;
+                             pl > (char *)a && cmp(pl - es, pl) > 0;
+                             pl -= es)
+                                swap(pl, pl - es);
                 return;
         }
 
-        pn = ((char *) a) + n * esa;
+        pn = (char *)a + n * es;
         r = CML_MIN(pa - (char *)a, pb - pa);
-        vecswap(a, pb - r, r / lda, lda);
-
-        tn = t + n * est;
-        r_t = CML_MIN(ta - t, tb - ta);
-        vecswapindex(t, tb - r_t, r_t / ldt, ldt);
-
-        r = CML_MIN(pd - pc, pn - pd - esa);
-        vecswap(pb, pn - r, r / lda, lda);
-
-        r_t = CML_MIN(td - tc, tn - td - est);
-        vecswapindex(tb, tn - r_t, r_t / ldt, ldt);
-
-        if ((r = pb - pa) > esa)
-                cml_qsort(a, r / esa, es, lda, t, ldt, index_flag, cmp);
-        if ((r = pd - pc) > esa)
-        {
+        vecswap(a, pb - r, r);
+        r = CML_MIN((size_t)(pd - pc), ((size_t)(pn - pd)) - es);
+        vecswap(pb, pn - r, r);
+        if ((size_t)(r = pb - pa) > es)
+                cml_qsort(a, r / es, es, cmp);
+        if ((size_t)(r = pd - pc) > es) {
                 /* Iterate rather than recurse to save stack space */
                 a = pn - r;
-                t = tn - (td - tc);
-                n = r / esa;
+                n = r / es;
                 goto loop;
         }
-        /* cml_qsort(pn - r, r / esa, es, lda, tn - (td - tc), ldt, cmp); */
+/*    cml_qsort(pn - r, r / es, es, cmp);*/
 }
-
-#undef swapcode
-#undef swap
-#undef vecswap
-#undef SWAPINIT
