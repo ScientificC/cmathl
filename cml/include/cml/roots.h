27fb0dad50cbdc2d2505a5ce9f69b91beb37fe68
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-29 20:37:33 -0300

    Added newton method

--- cml/include/cml/roots.h
+++ cml/include/cml/roots.h
@@ -11,26 +11,31 @@
 
 __CML_BEGIN_DECLS
 
-double cml_root_brent(cml_function_t *f, double x1, double x2, double *tol);
+double cml_root_brent(cml_function_t *func, double x1, double x2, double *tol);
 
-int cml_root_newton_bisection(cml_function_fdf_t *f, double x_min, double x_max,
+int cml_root_newton_bisection(cml_function_fdf_t *func, double x_min, double x_max,
                               double tol,int n_max,double * res);
-int cml_root_newton(cml_function_fdf_t *f, double x0, double epsrel,
-                    double epsabs, int n_max, double * res);
-int cml_root_bisection(cml_function_t *func, double xmin, double xmax,
+
+int cml_root_newton(cml_function_fdf_t *func, double x0,
+                    double epsrel, double epsabs,
+                    int n_max, double * res);
+
+int cml_root_bisection(cml_function_t *funcunc, double xmin, double xmax,
                        double epsrel, double epsabs,
                        size_t n_max, double *res);
 
-int cml_multiroot_newton(cml_function_vec_t *func, const double *x0, double x_eps,
-                         double fx_eps, int max_iter, int verbose, double *res);
+int cml_multiroot_newton(cml_function_vec_t *funcunc, const double *x0,
+                         double x_eps, double fx_eps, int n_max,
+                         bool verbose, double *res);
 
-int cml_root_fsolve(cml_function_vec_t *f, double *x, double *fx,
-                    double xtol, int maxfev, int *nfev, double
-                    *scale, int error_msg);
-int cml_root_fsolve_lsq(cml_function_vec_t *f, double *x, int m,
+int cml_root_fsolve(cml_function_vec_t *func, double *x, double *fx,
+                    double xtol, int maxfev, int *nfev,
+                    double *scale, int error_msg);
+
+int cml_root_fsolve_lsq(cml_function_vec_t *func, double *x, int m,
                         double *fx,  double xtol, double ftol,
-                        double gtol, int maxfev, int *nfev, double
-                        *scale, int error_msg);
+                        double gtol, int maxfev, int *nfev,
+                        double *scale, int error_msg);
 
 __CML_END_DECLS
 
