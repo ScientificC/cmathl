cb22e2eddf73cf1f92cd7c9ddf656d7f2bb5b079
ulises-jeremias <ulisescf.24@gmail.com>
2018-10-01 21:00:51 -0300

    Updated styles issues

--- cml/include/cml/quaternion/exponentiation.h
+++ cml/include/cml/quaternion/exponentiation.h
@@ -4,13 +4,13 @@
 __CML_BEGIN_DECLS
 
 void cml_quaternion_inplace_pow_scalar(cml_quaternion_t *q, double s);
-void cml_quaternion_inplace_pow(cml_quaternion_t *q, cml_quaternion_t q);
+void cml_quaternion_inplace_pow(cml_quaternion_t *q, cml_quaternion_t p);
 void cml_quaternion_inplace_scalar_pow(double s, cml_quaternion_t *q);
 
 cml_quaternion_t cml_quaternion_exp(cml_quaternion_t q);
 cml_quaternion_t cml_quaternion_log(cml_quaternion_t q);
 cml_quaternion_t cml_quaternion_pow_scalar(cml_quaternion_t q, double s);
-cml_quaternion_t cml_quaternion_pow(cml_quaternion_t q, cml_quaternion_t q);
+cml_quaternion_t cml_quaternion_pow(cml_quaternion_t q, cml_quaternion_t p);
 cml_quaternion_t cml_quaternion_scalar_pow(double s, cml_quaternion_t q);
 cml_quaternion_t cml_quaternion_sqrt(cml_quaternion_t q);
 
