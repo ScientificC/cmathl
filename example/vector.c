7e8831718accb7fe3d40194f53d8b6724bb7226c
ulises-jeremias <ulisescf.24@gmail.com>
2019-01-26 15:23:28 -0300

    Added minmax macros

--- example/vector.c
+++ example/vector.c
@@ -16,8 +16,8 @@ main(int argc, const char *argv[])
         vector_push_back(&vector, &x);
         vector_insert(&vector, 1, &y);
 
-        printf("Min element: %d\n", *((int *) cml_vector_min(&vector, &cml_sort_int_cmp)));
-        printf("Max element: %d\n", *((int *) cml_vector_max(&vector, &cml_sort_int_cmp)));
+        printf("Min element: %d\n", cml_vector_min_as(int, &vector, &cml_sort_int_cmp));
+        printf("Max element: %d\n", cml_vector_max_as(int, &vector, &cml_sort_int_cmp));
         
         vector_clear(&vector);
         vector_destroy(&vector);
